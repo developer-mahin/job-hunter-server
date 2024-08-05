a5b2247e086ac12f35844f52048e6a2c05b9c970
developer-mahin <mdmahin1310@gmail.com>
2024-08-05 21:57:01 +0600

    create update comment api

--- src/app/Modules/comment/comment.service.ts
+++ src/app/Modules/comment/comment.service.ts
@@ -6,6 +6,7 @@ import { decodeToken } from '../../../utils/decodeToken';
 import { TComment } from '../Post/post.interface';
 import Post from '../Post/post.model';
 import { TUpdateCommentData } from './comment.interface';
+import mongoose from 'mongoose';
 
 const createCommentIntoBD = async (
   id: string,
@@ -29,7 +30,7 @@ const createCommentIntoBD = async (
   };
 
   const result = await Post.updateOne(
-    { _id: id },
+    { _id: new mongoose.Types.ObjectId(id) },
     { $push: { comments: commentData } },
     { new: true },
   );
@@ -42,28 +43,57 @@ const updateCommentIntoDB = async (
   payload: TUpdateCommentData,
   token: string,
 ) => {
-  const postExist = await Post.findById(id).populate('author').populate({
-    path: 'comments.user',
-    model: 'User',
-  });
+  const postExist = await Post.findById(id);
+
   if (!postExist) {
     throw new AppError(httpStatus.NOT_FOUND, 'Post not found with this ID!!!');
   }
-
-  console.log(postExist);
 
   const decode = decodeToken(
     token,
     config.jwt.access_token as Secret,
   ) as JwtPayload;
 
-  const commentData = {
-    commentBody: payload.commentBody,
-    image: payload?.image || '',
-    user: decode.userId,
+  const userId = decode.userId;
+
+  // Find the specific comment
+  const post = await Post.findOne(
+    {
+      _id: id,
+      'comments._id': payload.commentId,
+      'comments.user': userId,
+    },
+    {
+      'comments.$': 1,
+    },
+  );
+
+  if (!post) {
+    throw new AppError(
+      httpStatus.FORBIDDEN,
+      "You can't update this comment!! you are not the author of this comment",
+    );
+  }
+
+  // Get the existing comment data
+  const existingComment = post?.comments?.[0];
+
+  // // Merge existing data with new data
+  const updatedCommentData = {
+    commentBody: payload?.commentBody || existingComment?.commentBody,
+    image: payload.image || existingComment?.image,
   };
 
-  // return result;
+  // // Update the specific comment with the merged data
+  await Post.updateOne(
+    { _id: id, 'comments._id': payload.commentId },
+    {
+      $set: {
+        'comments.$.commentBody': updatedCommentData.commentBody,
+        'comments.$.image': updatedCommentData.image,
+      },
+    },
+  );
 };
 
 export const commentService = {
